f1eb5facb9cd2b73e20b210b9ec727029620c5c4
skmutai6 <skmutai6@gmail.com>
2024-03-22 16:09:03 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide = function(){
+    let a = 2000;
+    let b = 100;
+    const divide = (a/b);
+    return 20;
+}
+
+const square = function(){
+    let x = 2;
+    const square = (x * x);
+    return 4;
+}
+
+const add = function(){
+    let a = 3;
+    let b = 4;
+    const add = (a + b);
+    return 7;
+}
